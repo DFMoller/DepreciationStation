b4bd688d30c118c3b986d49825d067a1435644d5
DFMoller <dfmoller@gmail.com>
2021-08-12 12:56:18 +0200

    Updated CSS

--- website/plotting.py
+++ website/plotting.py
@@ -13,21 +13,16 @@ def plot_snapshot(graph_data, year):
         foreground_strong='#EE6945',
         foreground_subtle='#A9BFD0',
         background='transparent',
-        plot_background='transparent'
+        plot_background='transparent',
+        colors=('#000000', '#dd2020', '#4078B7', '#545454', '#E98E19', '#A2A2A2', '#d2d2d2')
     )
 
-
-    # xy_chart = pygal.XY(style=custom_style, legend_at_bottom=True, legend_at_bottom_columns=len(years))
-    xy_chart = pygal.XY(style=custom_style, legend_at_bottom=True, stroke=False, dots_size=1.5)
+    xy_chart = pygal.XY(style=custom_style, legend_at_bottom=True, stroke=False, dots_size=1.4)
     xy_chart.title = f'Vehicles from {year} listed on Autotrader Today, sorted by colour'
     xy_chart.x_title = "Mileage (km)"
     xy_chart.y_title = "Value (Rand)"
 
-    # num_entries = 0
-
     for color in graph_data:
-        # num_entries += len(graph_data[color])
-        # if num_entries < 6000:
         xy_chart.add(color.capitalize(), graph_data[color])
 
     return xy_chart.render_data_uri()
@@ -40,28 +35,13 @@ def plot_timeline(graph_data, year):
         foreground_strong='#EE6945',
         foreground_subtle='#A9BFD0',
         background='transparent',
-        plot_background='transparent'
+        plot_background='transparent',
+        colors=('#000000', '#dd2020', '#4078B7', '#545454', '#E98E19', '#A2A2A2', '#d2d2d2')
     )
 
     all_dates = []
     final_data = {}
     max_val = 0;
-    # for color in graph_data:
-    #     df = DataFrame(graph_data[color], columns=["mileage", "value", "date"])
-    #     unique_dates = df["date"].unique()
-    #     line = []
-    #     for date in unique_dates:
-    #         filtered_df = df[df["date"] == date]
-    #         sorted_df = filtered_df.sort_values("value")
-    #         median_value = sorted_df["value"].median()
-    #         if median_value > max_val:
-    #             max_val = median_value
-    #         instance = (datetime.strptime(date, '%d/%m/%Y'), int(median_value))
-    #         line.append(instance)
-    #         date_obj = datetime.strptime(date, '%d/%m/%Y')
-    #         if date_obj not in all_dates:
-    #             all_dates.append(date_obj)
-    #     final_data[color] = line
 
     for color in graph_data:
         line = []
@@ -78,8 +58,6 @@ def plot_timeline(graph_data, year):
 
 
     dateline = pygal.DateLine(style=custom_style, legend_at_bottom=True, x_value_formatter=lambda dt: dt.strftime('%d/%m/%Y'), range=(0, max_val + 100000), min_scale=max_val*0.0001)
-    # xy_chart.x_value_formatter = lambda dt: str(dt)
-    # dateline.title = f'Vehicles from {year} || Historical Data'
     dateline.title = f'Daily median values of cars from {year}, per colour (Historical Data)'
     dateline.x_title = "Time"
     dateline.y_title = "Value (Rand)"
@@ -87,23 +65,21 @@ def plot_timeline(graph_data, year):
     for color in final_data:
         dateline.add(color.capitalize(), final_data[color])
 
-    # for col in final_data:
-    #     dateline.add(col, final_data[col])
-
     all_dates = sorted(all_dates)
     x_labels = []
-    # num_dates = len(all_dates)
+    num_dates = len(all_dates)
+    if num_dates > 8:
+        divider = round(num_dates/8, 0)
+    else:
+        divider = 1
     for count, date in enumerate(all_dates):
-        # if count % 2 == 0:
-        x_labels.append(date) # This is a datetime object
+        if count % divider == 0:
+            x_labels.append(date) # This is a datetime object
         # print(date)
 
     dateline.x_labels = x_labels
 
-    # dateline.x_value_formatter = lambda dt: '%s' % dt
-
     return dateline.render_data_uri()
-    # return None
 
 def remove_outliers(data):
     filtered_data = {}
